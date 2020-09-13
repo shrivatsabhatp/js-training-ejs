be1009e49278267ab1b6cfd6ee888f550d34b878
shrivatsaBhatP <shrivatsa@robosoftin.com>
2020-09-14 00:29:12 +0530

    chess board exercise from chapter 2

---
+++ chapter-02/ChessBoard.js
@@ -0,0 +1,16 @@
+let gridSize = 8;
+
+let board = "";
+
+for (let yAxis = 0; yAxis < gridSize; yAxis++) {
+    for (let xAxis = 0; xAxis < gridSize; xAxis++) {
+        if ((xAxis + yAxis) % 2 == 0) {
+            board += " ";
+        } else {
+            board += "#";
+        }
+    }
+    board += "\n";
+}
+
+console.log(board);
