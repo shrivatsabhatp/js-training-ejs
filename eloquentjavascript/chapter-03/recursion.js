2a67208c15afe12ee400d07aab35b9821da71b51
shrivatsaBhatP <shrivatsa@robosoftin.com>
2020-09-17 21:38:40 +0530

    updated comment of recursion

--- eloquentjavascript/chapter-03/recursion.js
+++ eloquentjavascript/chapter-03/recursion.js
@@ -1,7 +1,7 @@
 "use strict";
 // let isEven = number => number % 2 ? false : true;
 let isEven = number => (number >= 0) && (number % 2 ? false : true); 
-// check for ppositive number then check is even
+// check for ppositive number then check for even number
 
 console.log(isEven(75));
 console.log(isEven(50));
