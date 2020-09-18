3fd83674ec77203c15c6fdccf763b3e901e5ac35
shrivatsaBhatP <shrivatsa@robosoftin.com>
2020-09-18 09:46:34 +0530

    initial code

---
+++ exercise_1/test.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let square = x => x*x;
+
+function test(label, body) {
+    if (!body()) console.log(`Failed: ${label}`);
+}
+
+// Writing test cases
+
+test("Testing square of 2 returns 4", () => {
+    return square(2) === 4;
+});
+test("Testing square of 2 returns 5", () => {
+    return square(2) === 5;
+});
